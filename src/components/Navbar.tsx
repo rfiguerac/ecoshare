a26d8a2fc70c4bbe76c91a81ea2bc66d38fc7547
Andrés Rojas <99869407+andresrojasalzate@users.noreply.github.com>
2025-08-12 17:11:01 +0200

    Implement code changes to enhance functionality and improve performance

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,36 +9,37 @@ export const Navbar = () => {
           <ul
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-            <li><a>Item 1</a></li>
+            <li><a>Explore</a></li>
             <li>
-              <a>Parent</a>
+              <a>Map</a>
               <ul className="p-2">
                 <li><a>Submenu 1</a></li>
                 <li><a>Submenu 2</a></li>
               </ul>
             </li>
-            <li><a>Item 3</a></li>
+            <li><a>Saved</a></li>
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <img src="src\ecoshare-icon.png" width={50}/>
+        <a className="btn btn-ghost text-xl">EcoShare</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
-          <li><a>Item 1</a></li>
+          <li><a>Explore</a></li>
           <li>
             <details>
-              <summary>Parent</summary>
+              <summary>Map</summary>
               <ul className="p-2">
                 <li><a>Submenu 1</a></li>
                 <li><a>Submenu 2</a></li>
               </ul>
             </details>
           </li>
-          <li><a>Item 3</a></li>
+          <li><a>Saved</a></li>
         </ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <a className="btn"> + New donation</a>
       </div>
     </div>
   </>
