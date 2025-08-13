4e35e7613577b667db41dd4d654c83c9c739b75b
Andrés Rojas <99869407+andresrojasalzate@users.noreply.github.com>
2025-08-13 03:01:52 +0200

    implementado el buscador en el navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
+import { Search, Bell, User, MapPin, Funnel } from 'lucide-react';
 export const Navbar = () => {
   return <>
-    <div className="navbar bg-base-100 shadow-sm px-15">
+    <div className="navbar bg-base-100 shadow-sm">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -20,22 +21,58 @@ export const Navbar = () => {
             <li><a>Saved</a></li>
           </ul>
         </div>
-        <img src="src\assets\ecoshare-icon.png" width={50}/>
-        <a className="btn btn-ghost text-xl">EcoShare</a>
+        <div className="flex-none">
+          <img src="src\assets\ecoshare-icon.png" width={50} alt="EcoShare logo" />
+        </div>
+        <div className="flex-col items-start ml-4">
+          <a className="text-xl font-bold">EcoShare</a>
+          <p className="text-sm text-gray-500 mt-0.5">Reduce • Reuse • Share</p>
+        </div>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li><a><i className="fa-solid fa-magnifying-glass"></i> Explore</a></li>
-          <li><a><i className="fa-solid fa-location-dot"></i> Map</a></li>
-          <li><a><i className="fa-regular fa-heart"></i> Saved</a></li>
-        </ul>
+
+        <div className=" w-full max-w-4xl mx-auto">
+          <form action="" method="get" className="flex items-center w-full bg-white rounded-3xl shadow-lg p-2">
+
+            <div className="flex-shrink-0 p-2 text-gray-500">
+              <Search size={24} />
+            </div>
+
+            <input
+              type="text"
+              placeholder="Search for items, food, clothing..."
+              className="w-full h-10 px-4 text-gray-700 bg-transparent border-none focus:outline-none"
+            />
+
+            <div className="flex items-center p-2 text-gray-600 cursor-pointer hover:bg-gray-100 rounded-full transition-colors">
+              <MapPin strokeWidth={1.5} size={20} />
+              <span className="ml-1 text-sm font-medium hidden md:inline">Near me</span>
+            </div>
+
+            <div className="flex items-center p-2 text-gray-600 cursor-pointer hover:bg-gray-100 rounded-full transition-colors">
+              <Funnel strokeWidth={1.5} size={20} />
+              <span className="ml-1 text-sm font-medium hidden md:inline">Filters</span>
+            </div>
+
+            <input
+              type="submit"
+              value="Search"
+              className="btn ml-2 bg-[#28A745] text-white hover:bg-[#218838] rounded-full min-w-[100px] font-bold"
+            />
+          </form>
+        </div>
+        {/* <ul className="menu menu-horizontal px-1">
+        <li><a className="text-gray-500 font-bold"><Search size={18} />Explore</a></li>
+        <li><a className="text-gray-500 font-bold"><MapPin size={18} /> Map</a></li>
+        <li><a className="text-gray-500 font-bold"><Heart size={18} /> Saved</a></li>
+      </ul> */}
       </div>
       <div className="navbar-end gap-x-5">
-        <i className="fa-regular fa-bell"></i>
+        <Bell size={18} />
         <a className="btn bg-[#28A745] text-white hover:bg-[#218838]"> + New donation</a>
-        <i className="fa-regular fa-user"></i>
-      </div> 
-    </div> 
+        <User size={18} />
+      </div>
+    </div >
   </>
   {/* </> <div className="">Navbar</div>; */ }
 };
