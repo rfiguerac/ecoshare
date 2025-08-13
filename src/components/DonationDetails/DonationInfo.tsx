ba6493362b78133f15d0c57671242a541436b7e3
Andrés Rojas <99869407+andresrojasalzate@users.noreply.github.com>
2025-08-14 01:26:43 +0200

    creado el componente con la informacion de la donacion

---
+++ src/components/DonationDetails/DonationInfo.tsx
@@ -0,0 +1,32 @@
+import { Heart, Share2, MapPin, MessageCircle} from "lucide-react";
+
+export const DonationInfo = () => {
+
+    return <>
+        <div className="card bg-base-100 w-90 shadow-sm mt-4">
+            <figure>
+                <img
+                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                    alt="Shoes" />
+            </figure>
+            <div className="card-body">
+                <div className=" card-actions justify-end gap-5"> 
+                    <Heart/>
+                    <Share2/>
+                </div>
+                <h2 className="card-title">Card Title</h2>
+                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                <p className="flex gap-1 items-center text-xs mt-4"><MapPin/> 123 Green St, NYC</p>
+                <p className="font-bold mt-4">Donated by</p>
+                <div className="flex gap-2">
+                    <img src="src\assets\user-icono.png" alt="user icon" className="w-8 h-8 rounded-full"/>
+                    <p className="font-bold">Maria Gonzáles</p>
+                </div>
+                <div className="flex mt-4 gap-3">
+                    <button className="btn bg-[#28A745] text-white hover:bg-[#218838] px-10"> <MessageCircle size={15}/>Contact Dono</button>
+                    <button className="btn"> View on Map</button>
+                </div>
+            </div>
+        </div>
+    </>
+}
--- src/pages/DonationDetailsPage.tsx
+++ src/pages/DonationDetailsPage.tsx
@@ -1,6 +1,8 @@
+import { DonationInfo } from '../components/DonationDetails/DonationInfo';
+
 export const DonationDetailsPage = () => {
 
-  return <div>
-    Donation Page
+  return <div className="bg-[#EAF6EF] grid grid-cols-1 justify-items-center">
+    <DonationInfo/>
   </div>;
 };
