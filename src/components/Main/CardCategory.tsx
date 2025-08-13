1ec14b5f19bb54a39da95345520ca3f755be347d
McDavis Albertio <mcdavis705@gmail.com>
2025-08-13 16:42:07 +0200

    Created mock Featured Donations section

--- src/components/Main/CardCategory.tsx
+++ src/components/Main/CardCategory.tsx
@@ -12,7 +12,7 @@ interface CardCategoryProps {
 export const CardCategory = (props: CardCategoryProps) => {
   const { title, icon, } = props.category
   return (
-    <div className="card border border-gray-200 rounded-lg flex flex-col w-96 p-6 shadow-lg transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl hover:cursor-pointer">
+    <div className="card border border-gray-200 rounded-lg flex flex-col w-64 md:w-72  p-6 shadow-lg transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl hover:cursor-pointer flex-shrink-0">
       <div className={`w-25 h-25 mx-auto rounded-xl flex items-center justify-center mb-1 p-1 ${props.bgColor}`}>
         <DynamicIcon name={icon} size={50} />
       </div>
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,9 @@
 import BrowseCategory from "../components/Main/BrowseCategory";
+import FeaturedDonation from "../components/Main/FeaturedDonation";
 
 export const MainPage = () => {
   return <div>
     <BrowseCategory />
+    <FeaturedDonation />
   </div>;
 };
