82836de7cc073dbdb6bc798329805250c81f45a2
McDavis Albertio <mcdavis705@gmail.com>
2025-08-18 17:13:09 +0200

    Create routes and fixed the navbar at the top

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,48 +1,61 @@
 import { Search, Bell, User, Plus } from 'lucide-react';
+import { Link } from 'react-router-dom';
+
+
+
 export const Navbar = () => {
   return <>
-    <div className="navbar bg-base-100 shadow-sm lg:px-15">
-      <div className="navbar-start w-[30%] sm:w-1/2">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+    <div className=" flex navbar left-0 fixed bg-white/70 backdrop-blur-sm shadow-sm lg:px-15 z-50 top-0 w-full">
+      <div className="container mx-auto flex items-center justify-between">
+        <div className="navbar-start w-[30%] sm:w-1/2">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+              <li className='sm:hidden'><Link to="/CreateDonation">
+                <Plus /> New donation
+              </Link></li>
+              <li><a><Bell size={18} /> Notifications</a></li>
+              <li><a><User size={18} />Profile</a></li>
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-            <li className='sm:hidden'><a><Plus /> New donation</a></li>
-            <li><a><Bell size={18} /> Notifications</a></li>
-            <li><a><User size={18} />Profile</a></li>
-          </ul>
+          <Link to='/'>
+            <div className="flex-none">
+              <img
+                src="src\assets\ecoshare-icon.png" alt="EcoShare logo" className='w-10 md:w-12 lg:w-16' />
+            </div>
+          </Link>
+          <div className="flex-col items-start ml-4">
+            <a className="text-sm md:text-base lg:text-lg font-bold hidden lg:flex">EcoShare</a>
+            <p className="text-sm text-gray-500 mt-0.5 hidden lg:block">Reduce • Reuse • Share</p>
+          </div>
         </div>
-        <div className="flex-none">
-          <img src="src\assets\ecoshare-icon.png" alt="EcoShare logo" className='w-10 md:w-12 lg:w-16' />
-        </div>
-        <div className="flex-col items-start ml-4">
-          <a className="text-sm md:text-base lg:text-lg font-bold hidden lg:flex">EcoShare</a>
-          <p className="text-sm text-gray-500 mt-0.5 hidden lg:block">Reduce • Reuse • Share</p>
-        </div>
-      </div>
-      <div className="navbar-center">
+        <div className="navbar-center">
 
-        <div className="mx-auto">
-          <form action="" method="get" className="flex items-center bg-white border border-black rounded-3xl p-2">
-            <input
-              type="text"
-              placeholder="Search for items, food, clothing..."
-              className=" md:w-sm xl:min-w-md 2xl:min-w-3xl h-5 lg:h-10 px-4 text-sm md:text-base lg:text-lg text-gray-700 bg-transparent border-none focus:outline-none "
-            />
+          <div className="mx-auto ">
+            <form action="" method="get" className="flex items-center bg-white border border-black rounded-3xl p-2 ">
+              <input
+                type="text"
+                placeholder="Search for items, food, clothing..."
+                className=" md:w-sm xl:min-w-md 2xl:min-w-3xl h-5 lg:h-10 px-4 text-sm md:text-base lg:text-lg text-gray-700 bg-transparent border-none focus:outline-none "
+              />
 
-            <button type="submit" className="flex-shrink-0 p-2 text-gray-500">
-              <Search size={16} />
-            </button>
-          </form>
+              <button type="submit" className="flex-shrink-0 p-2 text-gray-500">
+                <Search size={16} />
+              </button>
+            </form>
+          </div>
         </div>
-      </div>
-      <div className="navbar-end gap-x-5 hidden sm:flex">
-        <a className="btn btn-sm md:btn-md bg-[#28A745] text-white hover:bg-[#218838]"> + New donation</a>
-        <Bell size={18} className='hidden lg:block' />
-        <User size={18} className='hidden lg:block' />
+        <div className="navbar-end gap-x-5 hidden sm:flex">
+          <Link to="/CreateDonation" className="btn btn-sm md:btn-md bg-[#28A745] text-white hover:bg-[#218838]">
+            + New donation
+          </Link>
+          <Bell size={18} className='hidden lg:block' />
+          <User size={18} className='hidden lg:block' />
+        </div>
       </div>
     </div >
   </>
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -6,14 +6,12 @@ export const MainLayout = () => {
   return (
     <div className="place-items-center">
       <main className="container">
-        <div className="pt-8">
+        <div className="pt-8 mb-5">
           <Navbar />
         </div>
-
-        <div className="container pt-2">
+        <div className="container pt-15">
           <Outlet />
         </div>
-
 
         {/* footer */}
         <div className="container pt-2">
