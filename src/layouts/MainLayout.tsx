ceee56107d45ea9164c091e4a494ff79d09ec4ee
Andrés Rojas <99869407+andresrojasalzate@users.noreply.github.com>
2025-08-21 05:58:42 +0200

    fix: update Navbar import path in MainLayout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import { Navbar } from "../components/Main/Navbar";
+import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Main/Footer";
 
 
