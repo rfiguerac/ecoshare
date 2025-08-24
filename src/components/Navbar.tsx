f52a2c09f4ba1315956fc62bf244260d33f95f09
Ricardo Figuera <ricardofiguera.c@gmail.com>
2025-08-24 15:38:19 +0200

    refactor: reorganizar el Sidebar y Navbar para mejorar la navegación y la gestión del estado

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useState, useRef, useEffect } from "react";
-import { Search, Bell, User, Plus, LogOut } from "lucide-react";
-import { Link, useNavigate } from "react-router-dom";
+import { Search, Bell, User, Plus, LogOut, Menu } from "lucide-react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { donations } from "../data/donations";
 import CreateDonationForm from "./Donation/CreateDonationForm";
 import CreateProfileForm from "./Profile/CreateProfileForm";
@@ -10,11 +10,15 @@ import { useAuthStore } from "../store/AuthStore";
 // Sugerencias de búsqueda simuladas
 const mockSuggestions = donations.map((donation) => donation.title);
 
-/**
- * Componente de la barra de navegación que incluye
- * un campo de búsqueda con sugerencias y navegación.
- */
-export const Navbar = () => {
+interface Props {
+  isOpenMenu: boolean;
+  setIsOpenMenu: (isOpen: boolean) => void;
+}
+
+export const Navbar = ({ isOpenMenu, setIsOpenMenu }: Props) => {
+  const location = useLocation();
+  console.log(location.pathname);
+
   const [query, setQuery] = useState("");
   const [isOpen, setIsOpen] = useState(false);
   const searchContainerRef = useRef<HTMLDivElement>(null);
@@ -22,15 +26,10 @@ export const Navbar = () => {
 
   const { isAuthenticated, logout } = useAuthStore();
 
-  // Filtra las sugerencias basadas en la consulta del usuario
   const filteredSuggestions = mockSuggestions.filter((item) =>
     item.toLowerCase().includes(query.toLowerCase())
   );
 
-  /**
-   * Cierra el menú desplegable si el usuario hace clic fuera
-   * del contenedor del buscador.
-   */
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (
@@ -46,10 +45,6 @@ export const Navbar = () => {
     };
   }, []);
 
-  /**
-   * Maneja la navegación a la página de búsqueda.
-   * @param {string} value El valor de la consulta a buscar.
-   */
   const handleSearch = (value: string) => {
     if (value.trim() !== "") {
       navigate(`/DonationSearch?query=${encodeURIComponent(value)}`);
@@ -57,10 +52,6 @@ export const Navbar = () => {
     }
   };
 
-  /**
-   * Maneja la selección de una sugerencia del menú desplegable.
-   * @param {string} value El valor de la sugerencia seleccionada.
-   */
   const handleSelect = (value: string) => {
     setQuery(value);
     handleSearch(value);
@@ -98,61 +89,24 @@ export const Navbar = () => {
 
   return (
     <>
-      <div className=" flex navbar left-0 fixed bg-white/70 backdrop-blur-sm shadow-sm lg:px-15 z-50 top-0 w-full">
+      <div className="flex navbar left-0 fixed bg-white/70 backdrop-blur-sm shadow-sm lg:px-15 z-50 top-0 w-full">
         <div className="container mx-auto flex items-center justify-between">
-          <div className="navbar-start w-[30%] sm:w-1/2">
-            <div className="dropdown dropdown-right">
-              <div
-                tabIndex={0}
-                role="button"
-                className="btn btn-ghost lg:hidden">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor">
-                  {" "}
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h8m-8 6h16"
-                  />{" "}
-                </svg>
-              </div>
-              <ul
-                tabIndex={0}
-                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow right-0">
-                <li className="sm:hidden">
-                  <button onClick={handleShowModal}>
-                    <Plus /> New donation
-                  </button>
-                </li>
-                <li>
-                  <button>
-                    <Bell size={18} /> Notifications
-                  </button>
-                </li>
-                <li>
-                  {isAuthenticated ? (
-                    <button onClick={handleLogout}>
-                      <LogOut size={18} /> LogOut
-                    </button>
-                  ) : (
-                    <button onClick={handleShowLoginModal}>
-                      <User size={18} /> login
-                    </button>
-                  )}
-                </li>
-              </ul>
-            </div>
+          {/* SECCIÓN IZQUIERDA */}
+          <div className="navbar-start w-auto flex items-center">
+            {location.pathname.startsWith("/dashboard") && (
+              <button
+                className="lg:hidden btn btn-square btn-ghost mr-2"
+                onClick={() => setIsOpenMenu(!isOpenMenu)}>
+                <Menu size={24} />
+              </button>
+            )}
+
             <Link to="/">
               <div className="flex-none">
                 <img
                   src="\src\assets\ecoshare-icon.png"
                   alt="EcoShare logo"
-                  className="w-10 md:w-12 lg:w-16"
+                  className="w-16 md:w-20 lg:w-20"
                 />
               </div>
             </Link>
@@ -165,6 +119,8 @@ export const Navbar = () => {
               </p>
             </div>
           </div>
+
+          {/* SECCIÓN CENTRAL */}
           <div className="navbar-center">
             <div className="mx-auto relative" ref={searchContainerRef}>
               <form
@@ -173,7 +129,7 @@ export const Navbar = () => {
                 <input
                   type="text"
                   placeholder="Search for items, food, clothing..."
-                  className=" md:w-sm xl:min-w-md 2xl:min-w-3xl h-5 lg:h-10 px-4 text-sm md:text-base lg:text-lg text-gray-700 bg-transparent border-none focus:outline-none "
+                  className="md:w-sm xl:min-w-md 2xl:min-w-3xl h-5 lg:h-10 px-4 text-sm md:text-base lg:text-lg text-gray-700 bg-transparent border-none focus:outline-none"
                   value={query}
                   onChange={(e) => {
                     setQuery(e.target.value);
@@ -222,31 +178,69 @@ export const Navbar = () => {
                 )}
             </div>
           </div>
-          <div className="navbar-end gap-x-5 hidden sm:flex">
-            <button
-              onClick={handleShowModal}
-              className="btn btn-sm md:btn-md btn-primary hover:btn-secondary active:btn-accent">
-              + New donation
-            </button>
-            <button className="btn btn-square btn-ghost">
-              <Bell size={18} className="hidden lg:block" />
-            </button>
-            {isAuthenticated ? (
+
+          {/* SECCIÓN DERECHA */}
+          <div className="navbar-end w-auto flex items-center gap-2">
+            {/* Menú hamburguesa SOLO en móviles */}
+            <div className="dropdown dropdown-end sm:hidden">
+              <div tabIndex={0} role="button" className="btn btn-ghost">
+                <Menu size={24} />
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                <li>
+                  <button onClick={handleShowModal}>
+                    <Plus /> New donation
+                  </button>
+                </li>
+                <li>
+                  <button>
+                    <Bell size={18} /> Notifications
+                  </button>
+                </li>
+                <li>
+                  {isAuthenticated ? (
+                    <button onClick={handleLogout}>
+                      <LogOut size={18} /> LogOut
+                    </button>
+                  ) : (
+                    <button onClick={handleShowLoginModal}>
+                      <User size={18} /> Login
+                    </button>
+                  )}
+                </li>
+              </ul>
+            </div>
+
+            {/* Botones visibles en pantallas medianas y grandes */}
+            <div className="hidden sm:flex gap-x-5">
               <button
-                className="btn btn-square btn-ghost"
-                onClick={handleLogout}>
-                <LogOut size={18} />
+                onClick={handleShowModal}
+                className="btn btn-sm md:btn-md btn-primary hover:btn-secondary active:btn-accent">
+                + New donation
               </button>
-            ) : (
-              <button
-                className="btn btn-square btn-ghost"
-                onClick={handleShowLoginModal}>
-                <User size={18} />
+              <button className="btn btn-square btn-ghost">
+                <Bell size={18} className="hidden lg:block" />
               </button>
-            )}
+              {isAuthenticated ? (
+                <button
+                  className="btn btn-square btn-ghost"
+                  onClick={handleLogout}>
+                  <LogOut size={18} />
+                </button>
+              ) : (
+                <button
+                  className="btn btn-square btn-ghost"
+                  onClick={handleShowLoginModal}>
+                  <User size={18} />
+                </button>
+              )}
+            </div>
           </div>
         </div>
       </div>
+
       {isModalOpen && <CreateDonationForm handleShowModal={handleShowModal} />}
       {isLoginModalOpen && (
         <LoginForm
--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,18 +1,25 @@
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "../components/Dashboard/Sidebar";
 import { Navbar } from "../components/Navbar";
+import { useState } from "react";
+
 export const DashboardLayout = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
-      <div className="">
-        <Navbar />
+      <div className="fixed top-0 z-50 w-full">
+        <Navbar isOpenMenu={isOpen} setIsOpenMenu={setIsOpen} />
       </div>
-      <div className="pt-20">
-        <Sidebar>
-          <div className="pl-4 pr-4">
+      <div className="flex mt-10 min-h-screen">
+        <div className="">
+          <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
+        </div>
+        <div className="flex-grow p-4 pt-20 lg:ml-52 lg:pt-16">
+          <main className="w-full h-full">
             <Outlet />
-          </div>
-        </Sidebar>
+          </main>
+        </div>
       </div>
     </>
   );
