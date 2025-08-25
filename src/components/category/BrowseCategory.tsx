b32779334aab8d992dcf96b07f376ec4acfefebe
McDavis Albertio <mcdavis705@gmail.com>
2025-08-25 19:57:48 +0200

    fixed category

--- src/components/category/BrowseCategory.tsx
+++ src/components/category/BrowseCategory.tsx
@@ -42,34 +42,3 @@ const BrowseCategory = () => {
 };
 
 export default BrowseCategory;
-import type { Category } from "../../domain/interfaces/Category";
-import {Link} from "react-router";
-
-import { DynamicIcon } from "../Main/DynamicIcon";
-
-interface CardCategoryProps {
-  category: Category;
-  quantity?: number;
-  bgColor?: string;
-}
-
-
-// filter category based on category clicked
-export const CardCategory = (props: CardCategoryProps) => {
-  const { title, icon } = props.category;
-
-  return (
-    <Link to={`/donationSearch?query=${title.toLowerCase()}`}>
-    <div className="card border border-gray-200 rounded-lg flex flex-col w-48 md:w-56 p-4 shadow-lg transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl hover:cursor-pointer flex-shrink-0">
-      <div
-        className={`w-16 h-16 mx-auto rounded-xl flex items-center justify-center mb-1 p-1 ${props.bgColor}`}>
-        <DynamicIcon name={icon} size={32} />
-      </div>
-      <div className="card-body items-center text-center">
-        <h2 className="card-title text-base">{title}</h2>
-        <p className="text-gray-600 text-sm">{props.quantity} Items</p>
-      </div>
-    </div>
-    </Link>
-  );
-};
