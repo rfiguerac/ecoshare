d5429116a1ca4543a7bfc6baae1d614ffb99ac4c
Andrés Rojas <99869407+andresrojasalzate@users.noreply.github.com>
2025-08-29 18:07:49 +0200

    feat: enhance AboutDonor component with user type and donation count logic

--- src/components/donor/AboutDonor.tsx
+++ src/components/donor/AboutDonor.tsx
@@ -1,10 +1,21 @@
 import { Star, CalendarDays } from 'lucide-react'
+import type { User } from '../../domain/interfaces/User';
+import { useDonationStore } from '../../store/DonationStore';
 
 interface AboutDonorProps {
-    user: any;
+    user: User;
 }
 
 export const AboutDonor = ({ user }: AboutDonorProps) => {
+
+    const { donationPagination, fetchDonations } = useDonationStore();
+    
+    const userDonations = donationPagination.data.reduce((totalDonations, donacionActual) => {
+        if (String(donacionActual.donorId) === String(user.id)) {
+            return totalDonations + 1;
+        }
+        return totalDonations;
+    }, 0)
     return <>
         <div className="card bg-base-100 w-90 shadow-sm mb-4 md:max-h-40">
             <p className='flex-start font-bold mt-4 ml-4'>About the Donor</p>
@@ -27,12 +38,12 @@ export const AboutDonor = ({ user }: AboutDonorProps) => {
                             ))}
                         </div>
                         <span className="font-semibold text-gray-800">{user.rating}</span>
-                        <span className="text-sm">• {user.donations}</span>
+                        <span className="text-sm">• {userDonations}</span>
                     </div>
 
                     <div className="flex items-center gap-2 text-gray-500">
                         <CalendarDays size={16} />
-                        <span className="text-sm">Joined {user.createdAt}</span>
+                        <span className="text-sm">Joined {user.createdAt ? new Date(user.createdAt).toDateString() : "N/A"}</span>
                     </div>
                 </div>
             </div>
